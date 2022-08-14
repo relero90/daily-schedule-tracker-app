fc17a2eb86e7c65ea8b5df22c50b6ea6e6f2ba60
relero90 <relero90@gmail.com>
2022-08-14 16:27:39 -0600

    finished dynamically styling rows based on relationship to current time

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,40 +1,69 @@
-// inserts currentDate as text content in jumbotron element
-var currentDate = moment();
-$("#currentDay").text(currentDate.format("MMMM Do, YYYY"));
-
-function pullExistingSchedule() {
-  // grab existing user inputs saves in localStorage
-  // render those inputs into textareas
-}
-pullExistingSchedule();
-
-// when user clicks on saveBtn...
+// Variable Delcarations
+var listItemEl = $(".row");
+var userDescr = $(".description");
 var saveBtn = $(".saveBtn");
+var currentDate = new Date().toLocaleDateString("en-us", {
+  weekday: "long",
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+});
+var currentHour = new Date().getHours();
+var savedData = [];
+
+function populateData() {
+  // populate #currentDay <p> element in header
+  $("#currentDay").text(currentDate);
+  // get saved user inputs from localStorage
+
+  // render those inputs into userDescr elements
+}
+populateData();
+
+// styles text areas based on the id's relationship to current hour
+for (var i = 0; i < listItemEl.length; i++) {
+  // if the id is equal to the current hour, add the class "present"
+  if (listItemEl[i].id == currentHour) {
+    userDescr[i].classList.add("present");
+    // if the id is less than the current hour, add the class "past"
+  } else if (listItemEl[i].id < currentHour) {
+    userDescr[i].classList.add("past");
+    // otherwise, add the class "future"
+  } else {
+    userDescr[i].classList.add("future");
+  }
+}
+
+// when user clicks on any saveBtn...
 saveBtn.on("click", function () {
-  //   capture user input in textarea and save to localStorage
+  //   for (var i = 0; i < saveBtn.length; i++) {}
+  //   capture user input in textarea & save to localStorage
+  captureUserInputs();
 });
 
-var currentTime = moment().format("LT");
-var hourLabel = $("label");
-console.log(hourLabel); //returns an object: label.hour
-
-// Maybe I want an array of the values?
-// var hourNum = [
-//   "9 AM",
-//   "10 AM",
-//   "11 AM",
-//   "12 PM",
-//   "1 PM",
-//   "2 PM",
-//   "3 PM",
-//   "4 PM",
-//   "5 PM",
-//   "6 PM",
-// ];
-
-// If conditionals something like this...
-// if (1 <= currentTime && currentTime < 2) {
-//   textarea.attr("class", present);
-// }
-// parseInt(hourLabel);
-// console.log(hourNum);
+function captureUserInputs() {
+  for (var i = 0; i < userDescr.length; i++) {
+    var savedEvent = userDescr.val();
+    savedData.push(savedEvent);
+    console.log(savedData[0]);
+    console.log(savedEvent);
+  }
+  //   var storedStringInput = JSON.stringify(savedScores);
+  //   localStorage.setItem("storedDataString", storedStringInput);
+}
+// Grabs stored score & initial information and adds it to the current savedScores array
+function renderScoreLog() {
+  scoreBoardEl.textContent = "";
+  // Grabs items from the "storedDataString" that are stored in local storage and changes them back into object items in the array savedScores
+  savedScores = JSON.parse(localStorage.getItem("storedDataString"));
+  // For each item in the savedScores array, create a list item equal to the value of that item; append the li to scoreBoardEl
+  for (var i = 0; i < savedScores.length; i++) {
+    // troubleshoot -
+    // array duplicates old entries when adding new ones (first submit results in 1 <li>, next one 2, then 3...)
+    var pulledScore = savedScores[i];
+    var printedScore = document.createElement("li");
+    printedScore.textContent = pulledScore;
+    printedScore.setAttribute("data-index", i);
+    scoreBoardEl.appendChild(printedScore);
+  }
+}
