47463eee59af5e120eaef570f5091334eb2e2bc4
relero90 <relero90@gmail.com>
2022-08-14 19:19:52 -0600

    built logic for capturing user input text and button number to localStorage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,9 @@
 // Variable Delcarations
 var listItemEl = $(".row");
+var timeLabelEl = $(".time");
 var userDescr = $(".description");
-var saveBtn = $(".saveBtn");
+var saveBtns = $(".saveBtn");
+var clickedBtn;
 var currentDate = new Date().toLocaleDateString("en-us", {
   weekday: "long",
   year: "numeric",
@@ -9,13 +11,12 @@ var currentDate = new Date().toLocaleDateString("en-us", {
   day: "numeric",
 });
 var currentHour = new Date().getHours();
-var savedData = [];
 
 function populateData() {
   // populate #currentDay <p> element in header
   $("#currentDay").text(currentDate);
   // get saved user inputs from localStorage
-
+  var savedData = localStorage.getItem();
   // render those inputs into userDescr elements
 }
 populateData();
@@ -34,40 +35,11 @@ for (var i = 0; i < listItemEl.length; i++) {
   }
 }
 
-// when user clicks on any saveBtn...
-saveBtn.on("click", function () {
-  //   for (var i = 0; i < saveBtn.length; i++) {}
-  var savedText = listItemEl.children().eq(1).val();
-  savedData.push(savedText);
-  console.log(savedData);
+// when user clicks on any saveBtn, capture user input in textarea & save to localStorage with a key of the btn id
+saveBtns.on("click", function (event) {
+  event.preventDefault();
+  // selects the target of the click event (the button)
+  var clickedBtn = event.target;
+  var userEventInput = clickedBtn.previousElementSibling;
+  localStorage.setItem(clickedBtn.id, userEventInput.value);
 });
-
-//   for (var i = 0; i < saveBtn.length; i++) {}
-//   capture user input in textarea & save to localStorage
-
-// function captureUserInputs() {
-//   for (var i = 0; i < userDescr.length; i++) {
-//     var savedEvent = userDescr.val();
-//     savedData.push(savedEvent);
-//     console.log(savedData[0]);
-//     console.log(savedEvent);
-//   }
-//   //   var storedStringInput = JSON.stringify(savedScores);
-//   //   localStorage.setItem("storedDataString", storedStringInput);
-// }
-// Grabs stored score & initial information and adds it to the current savedScores array
-// function renderScoreLog() {
-//   scoreBoardEl.textContent = "";
-//   // Grabs items from the "storedDataString" that are stored in local storage and changes them back into object items in the array savedScores
-//   savedScores = JSON.parse(localStorage.getItem("storedDataString"));
-//   // For each item in the savedScores array, create a list item equal to the value of that item; append the li to scoreBoardEl
-//   for (var i = 0; i < savedScores.length; i++) {
-//     // troubleshoot -
-//     // array duplicates old entries when adding new ones (first submit results in 1 <li>, next one 2, then 3...)
-//     var pulledScore = savedScores[i];
-//     var printedScore = document.createElement("li");
-//     printedScore.textContent = pulledScore;
-//     printedScore.setAttribute("data-index", i);
-//     scoreBoardEl.appendChild(printedScore);
-//   }
-// }
